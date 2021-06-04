5bb77d21c7b1759a71d6feab19545315138df798
Hector <Hector@DESKTOP-V6G2545>
2021-06-04 00:17:54 -0500

    Commit incial con eclipse. Close #1

---
+++ AppUsers/src/main/java/com/example/AppUsers/AppUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.AppUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppUsersApplication.class, args);
+	}
+
+}
